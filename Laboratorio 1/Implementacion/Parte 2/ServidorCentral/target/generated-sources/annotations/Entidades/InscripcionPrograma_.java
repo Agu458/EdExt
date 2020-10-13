b49bd79d71b5dab5c0958072afff5b4250221a63
Agu458 <66385798+Agu458@users.noreply.github.com>
2020-10-13 14:18:00 -0300

    ultimo

--- Laboratorio 1/Implementacion/Parte 2/ServidorCentral/target/generated-sources/annotations/Entidades/InscripcionPrograma_.java
+++ Laboratorio 1/Implementacion/Parte 2/ServidorCentral/target/generated-sources/annotations/Entidades/InscripcionPrograma_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-10-11T20:31:10")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-10-13T14:15:10")
 @StaticMetamodel(InscripcionPrograma.class)
 public class InscripcionPrograma_ { 
 
